7ee2ab12160f2fe63b9d20125e873c1286d2e2c5
minecraft <minecraft@centos6.(none)>
2015-12-28 09:25:07 -0700

    init

---
+++ filter.py
@@ -0,0 +1,57 @@
+import sys
+import subprocess
+
+PLAYERFILE = "/home/minecraft/known-players.txt"
+
+'''
+  EDIT HERE TO CHANGE MINECRAFT COMMAND
+'''
+def getSayCommand(uuid):
+    return "say Hello " + uuid
+
+def main():
+    while 1:
+        line = sys.stdin.readline()
+        if line == "":
+            break
+        elif "Starting minecraft" in line:
+            log("Scraping Started")
+        elif "UUID of player" in line:
+            uuid = line.split()[-1]
+
+            if not isKnownPlayer(uuid):
+                addNewPlayer(uuid)
+                executeNewPlayer(uuid)
+
+def log(s):
+    print ">>>>>>>>>>>>>>>>>>>>>FILTER.PY>>>>>>>>>>>>>>>>>>>>"
+    print s
+    print "<<<<<<<<<<<<<<<<<<<<<FILTER.PY<<<<<<<<<<<<<<<<<<<<"
+
+def isKnownPlayer(uuid):
+    try:
+        with open(PLAYERFILE, "r") as f:
+            for line in f:
+                if line.strip() == uuid:
+                    return True
+    except IOError:
+        return False
+
+    return False
+
+def addNewPlayer(uuid):
+    with open(PLAYERFILE, "a") as f:
+        f.write(uuid + "\n")
+
+    log("Added " + uuid)
+
+def executeNewPlayer(uuid):
+    subprocess.call([
+        'screen', '-S', 'minecraft', '-p', '0', '-X', 'stuff',
+        getSayCommand(uuid) + '\r'
+    ])
+    
+
+
+if __name__ == "__main__":
+    main()
